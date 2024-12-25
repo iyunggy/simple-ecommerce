06c358e48e8c331b869f75cdf15027328e74ea91
Muhammad Khoirul Anwar <muhammadkhoirulanwar@MacBook-Air-Muhammad.local>
2024-12-25 12:56:46 +0700

    ddelete backend

--- frontend/pages/lib/mongodb.js
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from "mongodb";
-
-export async function connectToMongoDB() {
-  const client = await MongoClient.connect(
-    "mongodb+srv://sindhueducourse:8XvBcR2pLN49VynS@cluster0.aam4u.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0",
-  );
-
-  return client;
-}
